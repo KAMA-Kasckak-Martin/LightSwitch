d8dbee4fe84f26fb017eb491c623ca57f984e540
Martin Kascak <m.kascak93@gmail.com>
2022-07-05 15:38:36 +0200

    finito

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+
+button.addEventListener("click", (e) => {
+  if (document.title === "Good Morning") {
+    document.title = "Good Night";
+  } else {
+    document.title = "Good Morning";
+  }
+  button.classList.toggle("button-dark");
+  body.classList.toggle("body-dark");
+});
